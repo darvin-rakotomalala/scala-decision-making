ef212a95c8f3c32933311f3a623d82e5d11db0b4
darvin <darvintojo@gmail.com>
2025-01-15 09:30:54 -0500

    Scala decision makin example

---
+++ src/main/scala/main.scala
@@ -0,0 +1,5 @@
+@main
+def main(): Unit =
+  println("----------------------------------------------------------------------------------")
+  println("Hello world! This an example to illustrate the Decision Making Statements in Scala")
+
